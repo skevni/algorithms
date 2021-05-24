ef389f619a763d427b9057c9416fbc4dcd0b217f
Evgeniy Sklyarov <skevni@gmail.com>
2021-05-25 02:19:05 +0300

    added description in comments

--- src/main/java/ru/geekbrains/sklyarov/lesson2/MainApp.java
+++ src/main/java/ru/geekbrains/sklyarov/lesson2/MainApp.java
@@ -10,9 +10,9 @@ public class MainApp {
             Unsorted array(100_000 rec). Duration of sorting by selection: 28009 ms
             Unsorted array(100_000 rec). Sort duration on insert: 33241 ms
         attempt #2
-            Unsorted array. Bubble sort duration: 100020 ms
-            Unsorted array. Duration of sorting by selection: 25794 ms
-            Unsorted array. Sort duration on insert: 29031 ms
+            Unsorted array(100_000 rec). Bubble sort duration: 100020 ms
+            Unsorted array(100_000 rec). Duration of sorting by selection: 25794 ms
+            Unsorted array(100_000 rec). Sort duration on insert: 29031 ms
          */
         UnsortedArrayAnalysis();
         /*
@@ -21,9 +21,9 @@ public class MainApp {
             Sorted array(100_000 rec). Duration of sorting by selection: 10694 ms
             Sorted array(100_000 rec). Sort duration on insert: 0 ms
         attempt #2
-            Sorted array. Bubble sort duration: 54370 ms
-            Sorted array. Duration of sorting by selection: 10748 ms
-            Sorted array. Sort duration on insert: 8 ms
+            Sorted array(100_000 rec). Bubble sort duration: 54370 ms
+            Sorted array(100_000 rec). Duration of sorting by selection: 10748 ms
+            Sorted array(100_000 rec). Sort duration on insert: 8 ms
          */
         sortedArrayAnalysis();
     }
